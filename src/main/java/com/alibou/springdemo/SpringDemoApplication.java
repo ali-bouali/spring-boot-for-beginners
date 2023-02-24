df93f263877f5b09b90535e10c77d9f3c65adec4
alibouali <ali.bouali@mimacom.com>
2023-02-24 14:05:44 +0100

    initializing spring boot project

---
+++ src/main/java/com/alibou/springdemo/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
